d18ddc07bed0e6e06cdd74a430c4291f5e4e126e
Saikat Sinha Ray <saikat.sinharay@gmail.com>
2025-07-03 21:24:42 +0530

    Update LogWatcher.py

--- logwatcher/LogWatcher.py
+++ logwatcher/LogWatcher.py
@@ -86,7 +86,7 @@ def search_files(directory, compiled_patterns):
                 continue
 
             filepath = os.path.join(root, file)
-            logger.debug(f"Parsing file: {filepath}")
+            logger.info(f"Parsing file: {filepath}")
             try:
                 with open(filepath, 'r', errors='ignore') as f:
                  for line_num, line in enumerate(f, 1):
