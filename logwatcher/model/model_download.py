2075efe068fb52845bed8bc09b4cf0fce379b749
Saikat Sinha Ray <saikat.sinharay@gmail.com>
2025-07-09 21:09:32 +0530

    Update model_download.py

--- logwatcher/model/model_download.py
+++ logwatcher/model/model_download.py
@@ -1,12 +1,15 @@
 from huggingface_hub import hf_hub_download
 import os
 import shutil
+import sys
 
 model_path = hf_hub_download(
     repo_id="saikat100/cvbert",
     filename="model.safetensors"
 )
+model_dir = os.path.join("/opt/LogWatcher","model")
 
-os.makedirs("logwatcher/model", exist_ok=True)
+print(f"Downloading the model at: {model_dir} ")
+os.makedirs(model_dir, exist_ok=True)
 shutil.copy(model_path, "model.safetensors")
 print("✅ Model downloaded")
