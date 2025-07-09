3db5f7aaa782c1baa22209a8fac3af60ffb7393a
Saikat Sinha Ray <saikat.sinharay@gmail.com>
2025-07-10 00:45:59 +0530

    Update LogWatcher.py

--- logwatcher/LogWatcher.py
+++ logwatcher/LogWatcher.py
@@ -160,10 +160,10 @@ def search_files(directory, compiled_patterns):
                             log(f"Matched line is part of the exclusion list. {filepath}:{line_num}:{line}", 2)
                             continue
 
-                    if classify_line(match_obj.group(0)) == "false_positive":
-                        log(f"CodeBERT marked as SAFE. Matched str: {match_obj.group(0)}, Full Line: {line}", 2)
+                    if classify_line(line) == "false_positive":
+                        log(f"CodeBERT marked as SAFE. Full Line: {line}", 2)
                         continue
-                    log(f"CodeBERT marked as UNSAFE. Matched str: {match_obj.group(0)}, Full Line: {line}", 1)
+                    log(f"CodeBERT marked as UNSAFE. Full Line: {line}", 1)
                     email_entry = f"{filepath}:{line_num}:{line}"
                     matches.append(email_entry)
                     for_csv_file[line] = line
