a4855d7349bce0bdf810c6851dc08a4bc2ab9911
Saikat Sinha Ray <saikat.sinharay@gmail.com>
2025-07-09 21:13:39 +0530

    Update model_download.py

--- logwatcher/model/model_download.py
+++ logwatcher/model/model_download.py
@@ -1,15 +1,24 @@
 from huggingface_hub import hf_hub_download
 import os
 import shutil
-import sys
+
+BASE_DIR = "/opt/LogWatcher"
+destination_dir = os.path.join(BASE_DIR, "model")
+destination_file = os.path.join(destination_dir, "model.safetensors")
+
+print(f"Downloading the model at: {destination_dir}")
+os.makedirs(destination_dir, exist_ok=True)
 
 model_path = hf_hub_download(
     repo_id="saikat100/cvbert",
     filename="model.safetensors"
 )
-model_dir = os.path.join("/opt/LogWatcher","model")
+print("Hugging Face cached path:", model_path)
 
-print(f"Downloading the model at: {model_dir} ")
-os.makedirs(model_dir, exist_ok=True)
-shutil.copy(model_path, "model.safetensors")
-print("✅ Model downloaded")
+shutil.copy(model_path, destination_file)
+print("Copied to:", destination_file)
+
+if os.path.exists(destination_file):
+    print("✅ Model downloaded successfully.")
+else:
+    print("❌ Model not found after copy.")
